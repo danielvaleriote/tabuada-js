7aad408dd78b7a621d04e4cffcf358151c8b3399
DanielValeriote <danielvaleriote@outlook.com>
2022-07-02 19:43:17 -0300

    add input validation and create other functions

--- script.js
+++ script.js
@@ -2,26 +2,40 @@
 
 const byId = id => document.getElementById(id); // Gets an element by ID
 
+const validateInput = ({value}) => {
+	if(value && Number(value) !== 0) return true;
+	return false;
+};
+
+const cleanInput = i => i.innerText = "";
+
 const pickNumberF = byId("pickNumberF");
 
 const answerForm = byId("answerForm");
 const answerInput = byId("answerInput");
 
-const pickIdInput = byId("pickIdInput");
+const pickNumberInput = byId("pickNumberInput");
 const quizWrapper = byId("quizWrapper");
 
 const firstN = byId("firstNumber"); //first number <span>
 const secondN = byId("secondNumber"); //second *
 
+var chosenNumber;
+var allQuestions;
+
 const pickNumber = e => {
 	e.preventDefault();
-	console.log(e);
+	if(!validateInput(pickNumberInput)) return;
+	chosenNumber = Number(pickNumberInput.value);
+	console.log(chosenNumber);
 };
 
 const submitAnswer = e => {
-	e.preventDefault()
-	console.log(e)
-}
+	e.preventDefault();
+	if(!validateInput(answerInput)) return;
+	const n = Number(answerInput.value);
+	console.log(n);
+};
 
 pickNumberF.addEventListener("submit", pickNumber);
 answerForm.addEventListener("submit", submitAnswer);
