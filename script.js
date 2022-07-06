8e8e833d72fb60fd53c7e3107db703ba95434e50
DanielValeriote <danielvaleriote@outlook.com>
2022-07-06 19:17:55 -0300

    fix modal bug and improve input focus

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 
 const byId = id => document.getElementById(id); // Gets an element by ID
 
-// input methods
+// Input methods
 const validateInput = ({value}) => value && Number(value) !== 0;
 const cleanInput = i => i.value = "";
 
-const quizWrapper = byId("quizWrapper")
+const quizWrapper = byId("quizWrapper");
+
 const pickNumberF = byId("pickNumberF");
-
 const answerForm = byId("answerForm");
-const answerInput = byId("answerInput");
 
+const answerInput = byId("answerInput");
 const pickNumberInput = byId("pickNumberInput");
 
 const multiplicands = [2,3,4,5,6,7,8,9];
@@ -31,9 +31,9 @@ const pickNumber = e => {
 	wrongAnswersCount = 0;
 	quizWrapper.style.display = "flex";
 
+	updateState("answering");
 	updateOperation();
 	cleanInput(pickNumberInput);
-	updateState("answering");
 	hideResult();
 };
 
@@ -41,12 +41,13 @@ const submitAnswer = e => {
 	e.preventDefault();
 	if(!validateInput(answerInput)) return;
 	const n = Number(answerInput.value);
+	answerInput.focus();
 	if(chosenNumber * currMultiplicand === n) {
-		// if the answer is right
+		// If the answer is right
 		displayAnswerResult(true);
 		updateOperation();
 	} else {
-		// if the answer is wrong
+		// If the answer is wrong
 		displayAnswerResult(false);
 		createModal("Resposta incorreta.", 1800);
 
@@ -62,12 +63,10 @@ const showResult = () => {
 	errMessageEl.innerHTML = `Total de erros: <strong>${wrongAnswersCount}</strong>`;
 	errMessageEl.style.color = wrongAnswersCount <= 2 ? "green" : wrongAnswersCount <= 4 ? "yellow" : "#dc3545"; 
 
-	updateState("idle")
+	updateState("idle");
 };
 
-const hideResult = () => {
-	byId("resultScreen").style.display = "none";
-}
+const hideResult = () => byId("resultScreen").style.display = "none";
 
 const displayAnswerResult = isAnswerRight => {
 	let cssClass;
@@ -86,31 +85,34 @@ const pickRandomMultiplicand = () => {
 };
 
 const updateOperation = () => {
-	if(currMultiplicands.length === 0) return showResult();
+	if(currMultiplicands.length === 0) {
+		pickNumberInput.focus();	
+		return showResult();
+	}
 
 	currMultiplicand = pickRandomMultiplicand();
 	byId("firstNumber").innerText = chosenNumber;
 	byId("secondNumber").innerText = currMultiplicand;
 };
 
-const updateState = (state) => {
+const updateState = state => {
 	switch(state) {
 		case "idle":
 			document.querySelector("#pickNumberF button").classList.remove("inactive-btn");
-			pickNumberInput.focus()
+			pickNumberInput.focus();
 			break;
 
 		case "answering":
 			document.querySelector("#pickNumberF button").classList.add("inactive-btn");
-			answerInput.focus()
+			answerInput.focus();
 			break;
-		}
-}
+		};
+};
 
 const createModal = (modalMessage, modalDuration, modalBgColor, modalTextColor) => {
 	// Create, define id and insert text inside modal
 	let modal = document.createElement("div");
-	modal.id = "modal"; modal.innerText = modalMessage;
+	modal.classList.add("modal"); modal.innerText = modalMessage;
 	// Modal custom style
 	if(modalBgColor) modal.style.backgroundColor = modalBgColor;
 	if(modalTextColor) modal.style.color = modalTextColor;
@@ -118,14 +120,13 @@ const createModal = (modalMessage, modalDuration, modalBgColor, modalTextColor) 
 	byId("modalContainer").appendChild(modal); 
 	// Deletes modal after specified time
 	if(Number(modalDuration))
-	setTimeout(() => deleteModal(), Number(modalDuration));
+	setTimeout(() => deleteModal(modal), Number(modalDuration));
 };
 
-const deleteModal = () => {
-	let modal = byId("modal");
-	// Slide modal out of the screen
+const deleteModal = modal => {
+	// Slide modal off the screen
 	modal.classList.add("slide-out-modal");
-	setTimeout(() => modal.remove(), 1800);
+	setTimeout(() => modal.remove(), 750);
 };
 
 pickNumberF.addEventListener("submit", pickNumber);
